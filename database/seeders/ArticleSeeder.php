6f344250429287fc7b9a68342a2c134fb8d01e91
Armanjafari <armanjafary1@gmail.com>
2020-12-26 02:56:49 +0330

    forgotten seeder

---
+++ database/seeders/ArticleSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\post;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ArticleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        foreach (range(1,10) as $item)
+        {
+            post::create([
+                'text' => "this a test number $item ",
+                'cat_id' => 1
+
+            ]);
+        }
+
+    }
+}
